944a57f80da3f5afa78f96eb1ea4d687a89d5699
abusazid46-tech <abusazid46@gmail.com>
2026-05-09 01:08:00 -0700

    Update __init__.py

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,3 +1,5 @@
+# backend/routes/__init__.py
+
 from .auth import router as auth_router
 from .user import router as user_router
 from .menu import router as menu_router
@@ -6,7 +8,7 @@ from .orders import router as order_router
 from .payment import router as payment_router
 from .reservation import router as reservation_router
 from .admin import router as admin_router
-from routes.admin_auth import router as admin_auth_router
+from .admin_auth import router as admin_auth_router  # Use relative import like others
 
 __all__ = [
     "auth_router",
@@ -16,6 +18,6 @@ __all__ = [
     "order_router",
     "payment_router",
     "reservation_router",
-    "admin_router"
+    "admin_router",  # ADDED MISSING COMMA HERE
     "admin_auth_router"
 ]
